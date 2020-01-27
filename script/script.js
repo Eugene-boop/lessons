f5de6712753d6282ccf77337f2db4b64851161a5
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-01-27 21:56:46 +0600

    Первая реализация на if и свиче

--- script/script.js
+++ script/script.js
@@ -1,16 +1,49 @@
-let money = 24000,
-  income = 'фриланс',
-  addExpenses = 'Интернет, Проезд, Тренажерка',
-  deposit = true,
-  mission = 100000,
-  period = 11,
-  budgetDay = money / 30;
+(()=> {
+  "use strict";
 
-console.log('money: ', typeof money);
-console.log('income: ', typeof income);
-console.log('deposit: ', typeof deposit);
-console.log('длина addExpenses:', addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-console.log('addExpenses как массив: ', addExpenses.toLowerCase().split(', '));
-console.log('budgetDay: ', budgetDay);
+  let money = prompt('Ваш месячный доход?', ''),
+    income = 'фриланс',
+    addExpenses = prompt('Перечислите возможные расходы зарассчитываемый период через запятую', ''),
+    deposit = !!confirm('Есть ли у вас депозит в банке'),
+    mission = 100000,
+
+    expenses1 = prompt('Введите обязательную статью расходов?', ''),
+    amount1 = +prompt('Во сколько это обойдется?', ''),
+    expenses2 = prompt('Введите обязательную статью расходов?', ''),
+    amount2 = +prompt('Во сколько это обойдется?', ''),
+
+    budgetMonth = +money - amount1 - amount2,
+    period = Math.ceil(mission / budgetMonth),
+    budgetDay = Math.floor(budgetMonth / 30);
+
+  console.log('money: ', typeof money);
+  console.log('income: ', typeof income);
+  console.log('deposit: ', typeof deposit);
+  console.log('длина addExpenses:', addExpenses.length);
+  console.log(`Период равен ${period} месяцев`);
+  console.log(`Цель заработать ${mission} рублей`);
+  console.log(addExpenses.toLowerCase().split(', '));
+  console.log('Бюджет на месяц: ', budgetMonth);
+  console.log('Бюджет на день: ', budgetDay);
+  console.log(`Цель будет достигнута за: ${period} месяцев`);
+  console.log('Бюджет на день: ', budgetDay);
+
+  if (budgetDay > 1200) {
+    console.log('У вас высокий уровень дохода');
+  } else if (budgetDay > 600 && budgetDay !== 1200) {
+    console.log('У вас средний уровень дохода');
+  } else if (budgetDay > 0 && budgetDay !== 600) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+  }
+  switch (budgetDay) {
+    case 1200:
+      console.log('Вы на пути к богатству, но провинциалы все еще тянут вас вниз');
+      break;
+    case 600:
+      console.log('Теперь вы гордый середнячок, не меньше, не больше!'); 
+      break;
+    case 0:
+      console.log('Ролтон и трубы ваш выбор!');
+  }
+
+})();
