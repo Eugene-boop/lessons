c10d8627c86af942824188a039337c6becf73094
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-01-29 01:44:36 +0600

    Подправил стрелочные

--- script/script.js
+++ script/script.js
@@ -19,7 +19,7 @@
     } else if (budgetDay >= 600) {
       return ('У вас средний уровень дохода');
     } else if (budgetDay > 0) {
-      return ('К сожалению у вас уровень дохода ниже среднего');
+      return ('К сожалению, у вас уровень дохода ниже среднего');
     } else {
       return ('Что-то пошло не так');
     }
@@ -33,10 +33,7 @@
     return money - getExpensesMonth();
   }
 
-  // получаю количество месяцев для накопления цели 
-  const getTargetMonth = () => {
-    return Math.floor(mission / accumulatedMonth);
-  };
+  const getTargetMonth = () => Math.floor(mission / accumulatedMonth);
 
   const showTypeOf = data => console.log(data, typeof(data));
 
