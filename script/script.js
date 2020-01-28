f9f9d794f7ee51adae90dfe4b83749fb54b321a6
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-01-28 21:07:23 +0600

    Подправил внешний вид

--- script/script.js
+++ script/script.js
@@ -1,15 +1,19 @@
 (()=> {
   "use strict";
-
   // 1 задание
-  let lang = prompt('Выберите язык en или ru', '');
+  let lang = prompt('Выберите язык en или ru', ''),
+      now = new Date(),
+      namePerson = prompt('Хто я?', ''),
+      dateArr = [
+        ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      ];
+  
   if (lang === 'ru') {
     console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
   } else if (lang === 'en') {
     console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
   }
-
-  lang = prompt('Выберите язык en или ru', '');
   switch (lang) {
     case 'ru':
       console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
@@ -17,19 +21,10 @@
     case 'en':
       console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
   }
-
-  //  магический многомерный массив остался мне не совсем понятен
-  lang = prompt('Выберите язык en или ru', '');
-  let dateArr = [
-    ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  ];
-  let now = new Date();
+  //  странная реализация через многомерный массив
   console.log(dateArr[+(lang === 'en')][now.getDay()]);
 
   // 2 задание
-  let namePerson = prompt('Хто я?', '');  
-
   (namePerson === 'Артем') ? console.log('директор') : 
   (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
 
