06e3222378e4deed8fa5b7f5d45c1e33829cc542
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-01-29 20:36:03 +0600

    Обрезающая функция

--- script/script.js
+++ script/script.js
@@ -1,48 +1,16 @@
 (()=> {
   "use strict";
 
-  let money = +prompt('Ваш месячный доход?', ''),
-    income = 'фриланс',
-    addExpenses = prompt('Перечислите возможные расходы зарассчитываемый период через запятую', '').toLowerCase().split(', '),
-    deposit = !!confirm('Есть ли у вас депозит в банке'),
-    mission = 100000,
-    expenses1 = prompt('Введите обязательную статью расходов?', ''),
-    amount1 = +prompt('Во сколько это обойдется?', ''),
-    expenses2 = prompt('Введите обязательную статью расходов?', ''),
-    amount2 = +prompt('Во сколько это обойдется?', ''),
-    accumulatedMonth = getAccumulatedMonth(),
-    budgetDay = Math.floor(accumulatedMonth / 30);
-
-  const getStatusIncome = () => {
-    if (budgetDay > 1200) {
-      return ('У вас высокий уровень дохода');
-    } else if (budgetDay >= 600) {
-      return ('У вас средний уровень дохода');
-    } else if (budgetDay > 0) {
-      return ('К сожалению, у вас уровень дохода ниже среднего');
+  const trim30 = string => {
+    if (typeof(string) !== 'string'){
+      return 'Введите строку!';
+    }
+    string = string.trim();
+    if (string.length > 30) {
+      return (string.slice(0,30) + '...');
     } else {
-      return ('Что-то пошло не так');
+      return (string);
     }
   };
-
-  function getExpensesMonth() {
-    return amount1 + amount2;
-  }
-
-  function getAccumulatedMonth() {
-    return money - getExpensesMonth();
-  }
-
-  const getTargetMonth = () => Math.floor(mission / accumulatedMonth);
-
-  const showTypeOf = data => console.log(data, typeof(data));
-
-  showTypeOf(money);
-  showTypeOf(income);
-  showTypeOf(deposit);
-  console.log('Сумма обязательных расходов', getExpensesMonth());
-  console.log(addExpenses);
-  console.log(`Цель будет достигнута за: ${getTargetMonth()} месяцев`);
-  console.log('Бюджет на день: ', budgetDay);
-  console.log( getStatusIncome() );
+  
 })();
