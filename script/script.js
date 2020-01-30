6186e3e7dcdec08cdfad16dc6159d6a6d11f3a8d
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-01-30 21:37:43 +0600

    Кривенько, с костылем и на циклах

--- script/script.js
+++ script/script.js
@@ -1,81 +1,24 @@
 (()=> {
   "use strict";
 
-  const isNumber = n => {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-  };
-
-  let money,
-    income = 'фриланс',
-    addExpenses = prompt('Перечислите возможные расходы зарассчитываемый период через запятую', '').toLowerCase().split(', '),
-    deposit = !!confirm('Есть ли у вас депозит в банке'),
-    mission = 100000,
-    expenses = [],
-    expensesAmount,
-    accumulatedMonth,
-    budgetDay;
-    // amount1 = +prompt('Во сколько это обойдется?', ''),
-    // amount2 = +prompt('Во сколько это обойдется?', ''),
-
-  const start = () => {
-    do {
-      money = prompt('Ваш месячный доход?', '');
-    } while (!isNumber(money));
-  };
-
-  start();
-
-  const getStatusIncome = () => {
-    if (budgetDay > 1200) {
-      return ('У вас высокий уровень дохода');
-    } else if (budgetDay >= 600) {
-      return ('У вас средний уровень дохода');
-    } else if (budgetDay > 0) {
-      return ('К сожалению, у вас уровень дохода ниже среднего');
-    } else {
-      return ('Что-то пошло не так');
+  // 1 задание
+  let arr = ['3494841839', '25019423', '981284856', '487922', '32975162', '57334982', '2786132'];
+  arr.forEach(item => {
+    if (item.startsWith('2') || item.startsWith('4')) {
+      console.log(item);
     }
-  };
+  });
 
-  function getExpensesMonth() {
-    let sum = 0, amount = 0;
-
-    for (let i = 0; i < 2; i++) {
-      expenses[i] = prompt('Введите обязательную статью расходов?', '');
-      do {
-      amount = prompt('Во сколько это обойдется?', '');
-      } while (!isNumber(amount));
-      sum += +amount;
+  // 2 задание
+  console.log(`Простые числа:`);
+  for (let i = 1; i <101; i++){
+    for (let j = 1; j <= i; j++) {
+      if (j === i) { 
+        console.log(`${i} - Делители этого числа: 1 и ${i}`);
+      } else if (i % j === 0 && j !== 1) {
+        break;
+      }
     }
-    return sum;
   }
-
-  expensesAmount = getExpensesMonth();
-
-  function getAccumulatedMonth() {
-    return (+money - expensesAmount);
-  }
-
-  accumulatedMonth = getAccumulatedMonth();
-  // budgetDay = Math.floor(accumulatedMonth / 30);
-
-  const getTargetMonth = () => {
-    if (accumulatedMonth < 0) {
-      return 'Цель не будет достигнута';
-    }
-    return `Цель будет достигнута за ${Math.floor(mission / accumulatedMonth)} месяца`;
-  };
-
-  const showTypeOf = data => console.log(data, typeof(data));
-
-  showTypeOf(money);
-  showTypeOf(income);
-  showTypeOf(deposit);
-  console.log('Дополнительные', addExpenses);
-  console.log('Обязательные расходы', expenses);
-  console.log('Сумма обязательных расходов', expensesAmount);
-  console.log(getTargetMonth());
-  // console.log('Бюджет на день: ', budgetDay); 
-  console.log( getStatusIncome() );
   
 })();
