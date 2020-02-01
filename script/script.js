b69ff7c8e9ae1333206341cdce73a9810f8576d2
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-02-01 16:01:41 +0600

    Работает

--- script/script.js
+++ script/script.js
@@ -1,24 +1,41 @@
-(()=> {
-  "use strict";
+(() => {
+  'use strict';
 
-  // 1 задание
-  let arr = ['3494841839', '25019423', '981284856', '487922', '32975162', '57334982', '2786132'];
-  arr.forEach(item => {
-    if (item.startsWith('2') || item.startsWith('4')) {
-      console.log(item);
-    }
-  });
+  const isNumber = n => {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
 
-  // 2 задание
-  console.log(`Простые числа:`);
-  for (let i = 1; i <101; i++){
-    for (let j = 1; j <= i; j++) {
-      if (j === i) { 
-        console.log(`${i} - Делители этого числа: 1 и ${i}`);
-      } else if (i % j === 0 && j !== 1) {
-        break;
+  const randNum = () => {
+    return Math.floor(1 + Math.random() * 100);
+  };
+
+  const start = () => {
+    let num = randNum();
+    
+    return function game() {
+      let answer = prompt('Угадай число от 1 до 100', '');
+
+      if (!isNumber(answer)) {
+        if (answer === null) { 
+          return 0; 
+        }
+        alert('Введи число!');
+      } else {
+        if (+answer === num) {
+          alert('Вы угадали!');
+          return 1;
+        } else if (num < +answer) {
+          alert('Загаданное число меньше');
+        } else if (num > +answer)  {
+          alert('Загаданное число больше');
+        } 
       }
-    }
-  }
+
+      game();
+    };
+  };
+
+  let riddle = start();
+  riddle();
   
 })();
