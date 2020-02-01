42c69e7b33cfaad073d7d54f28a3132e9da4bc22
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-02-01 22:39:05 +0600

    Игра

--- script/script.js
+++ script/script.js
@@ -1,41 +1,60 @@
 (() => {
   'use strict';
 
-  const isNumber = n => {
+  const isNum = n => {
     return !isNaN(parseFloat(n)) && isFinite(n);
-  };
+  }
 
   const randNum = () => {
     return Math.floor(1 + Math.random() * 100);
   };
 
-  const start = () => {
-    let num = randNum();
-    
+  const start = ((attempt = 10) => {
+    let num = randNum(),
+        attemptNow = 0;
+
     return function game() {
       let answer = prompt('Угадай число от 1 до 100', '');
-
-      if (!isNumber(answer)) {
+      
+      if (!isNum(answer)) {
         if (answer === null) { 
+          alert('Прощай(');
           return 0; 
         }
         alert('Введи число!');
       } else {
-        if (+answer === num) {
-          alert('Вы угадали!');
-          return 1;
+        if (num > +answer) {
+          alert(`Загаданное число больше, осталось попыток ${attempt - attemptNow - 1}`);
+          attemptNow += 1;
         } else if (num < +answer) {
-          alert('Загаданное число меньше');
-        } else if (num > +answer)  {
-          alert('Загаданное число больше');
+          alert(`Загаданное число меньше, осталось попыток ${attempt - attemptNow - 1}`);
+          attemptNow += 1;
+        } else if (+answer === num) {
+          if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) { 
+            num = randNum(); 
+            attemptNow = 0;
+            game(); 
+          }
+          alert('Прощай(');
+          return 1;
         } 
+      }
+
+      if (attempt === attemptNow) {
+        if (confirm('Попытки закончились, хотите еще?')) { 
+          num = randNum(); 
+          attemptNow = 0;
+          game(); 
+        }
+        alert('Прощай(');
+        return 0;
       }
 
       game();
     };
-  };
 
-  let riddle = start();
-  riddle();
-  
+  })();
+
+  start();
+
 })();
