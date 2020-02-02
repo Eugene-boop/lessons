0ade9920386f9cbae6630dca9d987b5fb24cd57f
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-02-02 15:00:37 +0600

    Костыльно и с переносами

--- script/script.js
+++ script/script.js
@@ -6,13 +6,13 @@
 
   for (let i = 1; i < week.length; i++) {
     if (now.getDay() === i || (i === 7 && now.getDay() === 0)) {
-      document.write(`<b>${week[i]}</b>`);
+      document.write(`<b>${week[i]}</b>` + '<br>');
       continue;
     }  else if (i === 7 || i === 6) {
-      document.write(`<i>${week[i]}</i>`);
+      document.write(`<i>${week[i]}</i>` + '<br>');
       continue;
     }
-    document.write(week[i]);
+    document.write(week[i] + '<br>');
   }
-  
+
 })();
