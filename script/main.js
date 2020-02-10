cad92df1f4a3c2db89f4e4b2154ce6728169637a
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-02-10 21:19:39 +0600

    скачет

--- script/main.js
+++ script/main.js
@@ -7,7 +7,8 @@
       hourVars = ['час', 'часа', 'часов'],
       minVars = ['минута', 'минуты', 'минут'],
       secVars = ['секунда', 'секунды', 'секунд'],
-      timeH1 = document.querySelector('.timer');
+      timerA = document.querySelector('.timer'),
+      timerB = document.querySelector('.timerB');
 
   function num2str(n, text_forms) {
     n = Math.abs(n) % 100;
@@ -28,10 +29,23 @@
     return text_forms[2];
   }
 
+  function addZero(item) {
+    if (item < 10) return '0' + item;
+    else return item;
+  }
+
   const showDate = () => {
-    let now = new Date();
-    timeH1.innerText = `Сегодня ${weekDays[now.getDay()]}, ${now.getDate()} ${months[now.getDate()]} ${now.getFullYear()} года, ${now.getHours()} 
-    ${num2str(now.getHours(), hourVars)} ${now.getMinutes()} ${num2str(now.getMinutes(), minVars)} ${now.getSeconds()} ${num2str(now.getSeconds(), secVars)}`;
+    let now = new Date(),
+        wd = now.getDay(),
+        dd = now.getDate(),
+        month = now.getMonth(),
+        yy = now.getFullYear(),
+        hh = now.getHours(),
+        mm = now.getMinutes(),
+        ss = now.getSeconds();
+
+    timerA.innerText = `Сегодня ${weekDays[wd]}, ${dd} ${months[month]} ${yy} года, ${hh} ${num2str(hh, hourVars)} ${mm} ${num2str(mm, minVars)} ${ss} ${num2str(ss, secVars)}`;
+    timerB.innerText = `${addZero(dd)}.${addZero(month + 1)}.${addZero(yy)} - ${addZero(hh)}:${addZero(mm)}:${addZero(ss  )}`;
   };
   setInterval(showDate, 1000);
 
