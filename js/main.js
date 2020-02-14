d407353544a0fc83e80cc75299cf2acc0bcb851e
Eugene Grigoriev <kivi200youtube@mail.ru>
2020-02-14 22:53:23 +0600

    оно заработалооо

--- js/main.js
+++ js/main.js
@@ -1,56 +1,106 @@
-// let car = {
-//   doors: 4,
-//   turbo: false,
-//   ride() {
-//     console.log('Трум-трум, ну я поехал');
-//   },
-// };
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+  let input = document.querySelector('.header-input'),
+    btn = document.querySelector('.header-button'),
+    todoItem = document.querySelector('.todo-item'),
+    itemsWrap = document.querySelector('.todo'),
+    completedItemsWrap = document.querySelector('.todo-completed'),
+    container = document.querySelector('.container'),
+    removeBtn = document.querySelector('.todo-remove'),
+    completeBtn = document.querySelector('.todo-complete');
 
-// let newCar = Object.create(car);  
-// newCar.model = 'mazda'
-// console.dir(newCar);
+  const addTodoItem = () => {
+    let newItem = todoItem.cloneNode(true);
+    newItem.childNodes[0].textContent = input.value;
+    input.value = '';
+    itemsWrap.insertAdjacentElement('afterbegin', newItem);
+    btn.disabled = true;
+  };
+  const checkInput = () => btn.disabled = !input.value.trim(); 
 
-// console.dir(car);
-// console.log(car.isPrototypeOf(newCar));
+  const removeItem = (e) => {
+    if (e.target.classList.contains('todo-remove')) {
+      e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+    }
+  };
 
-// function Car(model, color) {
-//   this.model = model;
-//   this.color = color;
-//   Car.prototype.ride = () => {
-//     console.log('Трум-трум, ну я поехал');
-//   };
-// }
+  const toggleComplete = (e) => {
+    if (e.target.classList.contains('todo-complete')) {
+      if (e.target.parentNode.parentNode.parentNode === itemsWrap) {
+        completedItemsWrap.insertAdjacentElement('afterbegin', e.target.parentNode.parentNode);
+      } else {
+        itemsWrap.insertAdjacentElement('afterbegin', e.target.parentNode.parentNode);
+      }
+    }
+  };
 
+  const addListeners = () => {
+    btn.addEventListener('click',(ev) => {
+      ev.preventDefault();
+      addTodoItem();
+    });
+    input.addEventListener('input', checkInput);
+    container.addEventListener('click', removeItem);
+    container.addEventListener('click', toggleComplete);
+    document.addEventListener('keydown', (e) => {
+      if (event.which === 13) addTodoItem();
+    });
+    window.addEventListener('beforeunload', () => setData());  
+  };
 
+  const setData = () => {
+    localStorage.removeItem('inputValue');
+    localStorage.removeItem('completedItems');
+    localStorage.removeItem('items');
 
-// let car1 = new Car('мазда', 'красный');
+    const completedItems = [],
+      completedList =  completedItemsWrap.querySelectorAll('li.todo-item');
+    completedList.forEach((item) => {
+      completedItems.push(item.childNodes[0].textContent);
+    }); 
+    
+    const items = [],
+      itemsList = itemsWrap.querySelectorAll('li.todo-item');
+    itemsList.forEach((item) => {
+      items.push(item.childNodes[0].textContent);
+    });
+      
+    localStorage.setItem('inputValue', input.value);
+    localStorage.setItem('completedItems', completedItems);
+    localStorage.setItem('items', items);
+  };
 
-// car1.ride();
+  const renderItems = () => {
+    input.value = localStorage.getItem('inputValue');
+    let items = localStorage.getItem('items');
+    let completedItems = localStorage.getItem('completedItems');
+    if (items) {
+      itemsWrap.innerHTML = '';
+      items = localStorage.getItem('items').split(',');
+      items.forEach((item) => {
+        let newItem = todoItem.cloneNode(true);
+        newItem.childNodes[0].textContent = item;
+        itemsWrap.insertAdjacentElement('beforeend', newItem);
+      });
+    } else itemsWrap.innerHTML = '';
+    if (completedItems) {
+      completedItemsWrap.innerHTML = '';
+      completedItems = localStorage.getItem('completedItems').split(',');
+      completedItems.forEach((item) => {
+        let newItem = todoItem.cloneNode(true);
+        newItem.childNodes[0].textContent = item;
+        completedItemsWrap.insertAdjacentElement('beforeend', newItem);
+      });
+    } else completedItemsWrap.innerHTML = '';
+  };
 
-// console.dir(car1);
+  const start = () => {
+    renderItems();
+    checkInput();
+    addListeners();   
+  };
 
-// console.dir(Car);
+  start();
 
-function Car(country, options) {
-  this.country = country;
-  options = options || {};
-  this.color = options.color;
-  this.trans = options.trans;
-}
+});
 
-Car.prototype.ride = function() {
-  console.log(this.brand + ' ' + this.model + ' поехала!');
-};
-
-Audi.prototype = Object.create(Car.prototype);
-
-function Audi(country, options, model, type) {
-  this.brand = 'Audi';
-  Car.apply(this, arguments);
-  this.model = model;
-  this.type = type;
-}
-
-let car_q7 = new Audi('Germany', {color: 'red'}, 'Q7', 'S');
-
-console.dir(car_q7);
